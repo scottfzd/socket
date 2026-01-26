a41a6773954bebf60bd711a7f7dfe5ebde68ca3a
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2026-01-26 21:49:41 +0100

    socket programming: create TCP socket with IPV4 address + bind socket to address (0.0.0.0, 8080) + put socket into LISTEN state (to accept incoming connections) + accept a client connection

---
+++ server.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+
+int main() {
+
+  // Create socket
+  int server_fd;
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (server_fd < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+
+
+  // Socket address
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr)); 
+  
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(8080);
+
+
+  // Bind socket to the address
+  bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+  // Listen for incoming connections
+  listen(server_fd, 5);
+
+
+  // Accept a client connection
+
+  struct sockaddr_in client_addr;
+  socklen_t client_len = sizeof(client_addr);
+
+  int client_fd = accept(server_fd, (struct  sockaddr *)&client_addr, &client_len);
+}
