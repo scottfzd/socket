3823cc5e71709b2f4733ac421861bfa8cf89a7fe
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2026-02-05 17:25:58 +0100

    refactor: retry connection if failure

--- client.c
+++ client.c
@@ -8,30 +8,34 @@
 
 SOCKET connect_to_server() {
 
-  SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+  while (1) {
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
 
-  if (sock == INVALID_SOCKET) {
-    printf("Could not create socket. Error: %d\n", WSAGetLastError());
-    return INVALID_SOCKET;
+    if (sock == INVALID_SOCKET) {
+      printf("Could not create socket. Error: %d\n", WSAGetLastError());
+      Sleep(5000);
+      continue;
+    }
+
+    // Server address to connect to
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    inet_pton(AF_INET, "192.168.209.1", &server_addr.sin_addr);
+
+    // Connect to remote service
+    if (connect(sock, &server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+      
+      printf("Unable to connect: %d\n", WSAGetLastError());
+      closesocket(sock);
+      Sleep(5000);
+      continue;
+    }
+
+    return sock;
   }
-
-  // Server address to connect to
-  struct sockaddr_in server_addr;
-  memset(&server_addr, 0, sizeof(server_addr));
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(8080);
-  inet_pton(AF_INET, "192.168.209.1", &server_addr.sin_addr);
-
-  // Connect to remote service
-  if (connect(sock, &server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-    
-    printf("Unable to connect: %d\n", WSAGetLastError());
-    closesocket(sock);
-    return INVALID_SOCKET;
-  }
-
-  return sock;
 }
 
 int main() {
@@ -105,4 +109,6 @@ int main() {
       }
     }
   }
+
+  WSACleanup();
 }
